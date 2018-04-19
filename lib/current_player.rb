c3886c55d90408c5ffc57f34cee6df154acb8e4b
Jordan Schultz <jpas97@gmail.com>
2018-04-19 14:24:15 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn.downcase == "x" || turn.downcase == "o"
+    counter += 1
+    puts "#{counter}"
+    end
+  end
+counter
+end
+
+def current_player(board)
+  if turn_count(board).even? || turn_count(board) == 0
+    return "X"
+  else
+    return "O"
+  end
+end
